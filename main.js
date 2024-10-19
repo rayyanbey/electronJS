2854237bf0dc9db97cf49bbdded73681af094990
Muhammad Rayyan Asghar <rayyanasghar9@gmail.com>
2024-10-19 19:45:24 +0500

    Refactor file structure and update main window
    - Move index.html to client/theme/index.html
    - Delete client/index.html
    - Add about.html to client/theme/about.html
    - Update main.js to load the new file paths
    - Update script.js path in client/theme/js
    - Update logo.svg path in client/theme/images
    - Update package.json configuration for Electron setup
    - Update Electron window width in main.js

--- main.js
+++ main.js
@@ -1,19 +1,40 @@
 import { app, BrowserWindow} from "electron";
 import path from "path";
+
+
+const isMac = process.platform === "darwin";
+const isDev = process.env.NODE_ENV !== "production";
+
+
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: "ImageShrink",
-    width: 500,
+    width: isDev? 1000: 500,
     height: 600
   });
 
+  //open dev tool if in dev env
+  if(isDev){
+    mainWindow.webContents.openDevTools();
+  }
   //mainWindow.loadURL('https://portfolio-hazel-xi-80.vercel.app/')
-  mainWindow.loadFile("client/index.html");
+  mainWindow.loadFile("client/theme/index.html");
 }
-
-
 
 app.whenReady().then(()=>{
     createMainWindow();
 }); 
 
+
+app.on("window-all-closed", () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+
+
+app.on("activate",()=>{
+  if(BrowserWindow.getAllWindows().length === 0){
+    createMainWindow();
+  }
+})
