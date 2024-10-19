04104a7b7d5028dc749b711bb22f6e8f73a3f231
Muhammad Rayyan Asghar <rayyanasghar9@gmail.com>
2024-10-19 16:52:51 +0500

    created the main window

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
 import { app, BrowserWindow} from "electron";
+import path from "path";
+function createMainWindow() {
+  const mainWindow = new BrowserWindow({
+    title: "ImageShrink",
+    width: 500,
+    height: 600
+  });
+
+  //mainWindow.loadURL('https://portfolio-hazel-xi-80.vercel.app/')
+  mainWindow.loadFile("client/index.html");
+}
+
+
+
+app.whenReady().then(()=>{
+    createMainWindow();
+}); 
 
